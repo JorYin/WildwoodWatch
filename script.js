a8200f743f7052325cc8fc501c1f653150a53f8f
JorYin <134739719+JorYin@users.noreply.github.com>
2023-10-30 13:45:09 -0400

    Form validation

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
+// Html Parts
+
+// Btn
 const themeButton = document.getElementById("dark-mode-btn");
-const signNowButton = document.getElementById("sign-btn")
-const signatures = document.getElementById("signatures")
-const signName = document.getElementById("inputName")
-const signHometown = document.getElementById("inputHometown")
-const signEmail = document.getElementById("inputEmail")
+const signNowButton = document.getElementById("sign-btn");
 
-signNowButton.addEventListener("click", e =>{
-  e.preventDefault();
-  formSignatures();
-});
+// Input
+const petitionInputs = document.getElementById("form-sign");
+const signatures = document.getElementById("signatures");
+const signName = document.getElementById("inputName");
+const signHometown = document.getElementById("inputHometown");
+const signEmail = document.getElementById("inputEmail");
 
-function formSignatures(){
-  let name = signName.value;
-  let homeTown = signHometown.value;
-  let email = signEmail.value;
-  
+// Counter
+const countSig = document.getElementById("count-sig");
 
-  const newPerson = document.createElement("p");
-  newPerson.innerHTML = `🖊️${name} from ${homeTown} supports this`;
-  signatures.appendChild(newPerson);
+// JS variables
+let count = 5;
 
-  if (signatures.length >= 6){
-
-  };
-
-
-};
-
-
+// Events
 themeButton.addEventListener("click", e =>{
   e.preventDefault();
 
   document.body.classList.toggle("dark-mode");
 });
+
+
+// Functions
+function formSignatures(){
+  let name = signName.value;
+  let homeTown = signHometown.value;
+  
+  if (signatures.children.length >= 6){
+    count += 1;
+    document.getElementById("petition-count").innerHTML = count;
+  } 
+  else{
+    const newPerson = document.createElement("p");
+    newPerson.innerHTML = `🖊️${name} from ${homeTown} supports this`;
+    signatures.insertBefore(newPerson, countSig)
+  }
+
+};
+
+// TODO: Complete validation form
+
+const validateForm = () => {
+
+  let containsErrors = false;
+  var petitionValues = petitionInputs.elements;
+
+  for (let i = 0; i < petitionValues.length; i++){
+    if (petitionValues[i].value.length < 2){
+      containsErrors = true;
+      petitionValues[i].classList.add("error");
+    } else{
+      petitionValues[i].classList.remove("error");
+    }
+  }
+  
+  if (containsErrors == false){
+    formSignatures();
+    for (let i = 0; i< petitionValues.length; i++){
+      petitionValues[i].value = "";
+      }
+  }
+}
+
+signNowButton.addEventListener("click", validateForm);
