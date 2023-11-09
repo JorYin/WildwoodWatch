3e4a9f1dd8fc5189d23399ee1297de7f9939cd06
JorYin <134739719+JorYin@users.noreply.github.com>
2023-11-09 15:59:43 -0500

    Modal

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // Btn
 const themeButton = document.getElementById("dark-mode-btn");
 const signNowButton = document.getElementById("sign-btn");
+const closeModalButton = document.getElementById("close-modal");
 
 // Input
 const petitionInputs = document.getElementById("form-sign");
@@ -10,6 +11,9 @@ const signatures = document.getElementById("signatures");
 const signName = document.getElementById("inputName");
 const signHometown = document.getElementById("inputHometown");
 const signEmail = document.getElementById("inputEmail");
+const modal = document.getElementById("thanks-modal");
+const modalContent = document.getElementById("thanks-modal-content")
+const modalImage = document.getElementById("modalImg")
 
 // Sections
 const revealableContainers = document.querySelectorAll(".revealable")
@@ -20,6 +24,8 @@ const countSig = document.getElementById("count-sig");
 
 // JS variables
 let count = 0;
+let scaleFactor = 1;
+
 
 let animation = {
   revealDistance : 150,
@@ -35,15 +41,20 @@ themeButton.addEventListener("click", e =>{
   e.preventDefault();
 
   document.body.classList.toggle("dark-mode");
+  
 });
 
 window.addEventListener("scroll", reveal);
 
+closeModalButton.addEventListener("click", e =>{
+  e.preventDefault();
+
+  modal.style.display = "none";
+});
+
 
 // Functions
-function formSignatures(){
-  let name = signName.value;
-  let homeTown = signHometown.value;
+function formSignatures(person){
   
   if (signatures.children.length >= 7){
     count += 1;
@@ -51,7 +62,7 @@ function formSignatures(){
   } 
   else{
     const newPerson = document.createElement("p");
-    newPerson.innerHTML = `🖊️${name} from ${homeTown} supports this`;
+    newPerson.innerHTML = `🖊️${person.name} from ${person.homeTown} supports this`;
     signatures.insertBefore(newPerson, countSig)
     count += 1;
     document.getElementById("petition-count").innerHTML = count;
@@ -79,12 +90,40 @@ function reveal() {
 
 }
 
-// TODO: Complete validation form
+function toggleModal(person){
 
+  modal.style.display = "flex";
+
+  modalContent.textContent = `Thank you, ${person.name} from ${person.homeTown}, for your support!`;
+
+  intervalId = setInterval(scaleImage, 500)
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+    modal.style.display = "none";
+  }, 4000);
+
+}
+
+function scaleImage() {
+  scaleFactor = scaleFactor === 1 ? 0.8 : 1
+
+  modalImage.style.transform = `scale(${scaleFactor})`;
+
+}
+
+// Form validation
 const validateForm = () => {
 
   let containsErrors = false;
-  var petitionValues = petitionInputs.elements;
+  
+  let petitionValues = petitionInputs.elements;
+
+  let person = {
+    name: petitionInputs[0].value, // accesses and saves value of first input
+    homeTown: petitionInputs[1].value,
+    email: petitionInputs[2].value
+  }
 
   for (let i = 0; i < petitionValues.length; i++){
     
@@ -95,7 +134,7 @@ const validateForm = () => {
       petitionValues[i].classList.remove("error");
     }
     
-    if (!signEmail.value.includes(".com")){
+    if (!person.email.includes(".com")){
       containsErrors = true;
       signEmail.classList.add('error');
     } else{
@@ -103,8 +142,10 @@ const validateForm = () => {
     }
   }
   
-  if (containsErrors == false){
-    formSignatures();
+  if (!containsErrors){
+    formSignatures(person);
+
+    toggleModal(person);
     for (let i = 0; i< petitionValues.length; i++){
       petitionValues[i].value = "";
       }
