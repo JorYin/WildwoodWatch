717a88d5248eda662d049c331d46b5f4554b7179
JorYin <134739719+JorYin@users.noreply.github.com>
2023-11-12 17:52:56 -0500

    Mobile Responsive

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const themeButton = document.getElementById("dark-mode-btn");
 const signNowButton = document.getElementById("sign-btn");
 const closeModalButton = document.getElementById("close-modal");
+const hamMenu = document.getElementById("menu-icon");
+const closeHamMenu = document.getElementById("close-icon");
 
 // Input
 const petitionInputs = document.getElementById("form-sign");
@@ -14,10 +16,12 @@ const signEmail = document.getElementById("inputEmail");
 const modal = document.getElementById("thanks-modal");
 const modalContent = document.getElementById("thanks-modal-content")
 const modalImage = document.getElementById("modalImg")
+const navBarMobile = document.querySelector(".nav-items-mobile")
 
 // Sections
 const revealableContainers = document.querySelectorAll(".revealable")
 const darkHero = document.querySelector(".background-container")
+const navLinks = document.querySelectorAll('.nav-items-mobile ul li a');
 
 // Counter
 const countSig = document.getElementById("count-sig");
@@ -38,23 +42,52 @@ let animation = {
 
 // Events
 themeButton.addEventListener("click", e =>{
-  e.preventDefault();
 
   darkHero.classList.toggle("dark-mode-background");
   
   document.body.classList.toggle("dark-mode");
   
-
 });
 
 window.addEventListener("scroll", reveal);
 
 closeModalButton.addEventListener("click", e =>{
-  e.preventDefault();
 
   modal.style.display = "none";
+
 });
 
+window.addEventListener("resize", e => {
+  if (window.innerWidth > 992) {
+    hamMenu.style.display = "none";
+    closeHamMenu.style.display = "none";
+  }
+});
+
+window.addEventListener("resize", e => {
+  if (window.innerWidth < 992) {
+    hamMenu.style.display = "block";
+    closeHamMenu.style.display = "none";
+  }
+});
+
+hamMenu.addEventListener('click', e => {
+  
+  navBarMobile.classList.add('nav-items-active');
+  hamMenu.style.display = "none";
+  closeHamMenu.style.display = "block";
+
+});
+
+closeHamMenu.addEventListener('click', e => {
+  
+  navBarMobile.classList.remove('nav-items-active');
+  hamMenu.style.display = "block";
+  closeHamMenu.style.display = "none";
+
+});
+
+navLinks.forEach(closeMenuLinks);
 
 // Functions
 function formSignatures(person){
@@ -113,6 +146,14 @@ function scaleImage() {
 
   modalImage.style.transform = `scale(${scaleFactor})`;
 
+}
+
+function closeMenuLinks(links) {
+  links.addEventListener("click", e => {
+    navBarMobile.classList.remove('nav-items-active');
+    hamMenu.style.display = "block";
+    closeHamMenu.style.display = "none";
+  })
 }
 
 // Form validation
